d42827afc9250eb72a87e4fb558f58a3ad779772
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-09-29 19:02:45 +0900

    * ncl-doc.el: just the begin.. nothing inside

---
+++ ncl-doc.el
@@ -0,0 +1,21 @@
+;;; ncl-doc.el
+;;
+;; Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
+;;    File: ncl-doc.el
+;;  Author: Yagnesh Raghava Yakkala <yagnesh@NOSPAM.live.com>
+;; Created: Saturday, September 24 2011
+;; License: GPL v3 or later. <http://www.gnu.org/licenses/gpl.html>
+
+;;; Description:
+;; this is a minor moder to look
+;; ncl-doc-thing-at-point => collects the appropriate thing at point
+;; ncl-doc-classify => should look where to look
+;; ncl-doc-fetch => bring the doc from server (local?)
+;; ncl-doc-crop-header => should crop the headers
+;; ncl-doc-render => should render html doc
+
+;;; code starts here
+
+
+(provide 'ncl-doc)
+;;; ncl-doc.el ends here
