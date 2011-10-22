0da34a67d5648f85cf9141953cea8d96fc0ecf79
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-10-22 17:20:37 +0900

    * ncl-doc.el: few variables and functions defined

--- ncl-doc.el
+++ ncl-doc.el
@@ -19,8 +19,19 @@
 ;;; requires
 (require 'ncl)                          ; to bring all the
                                         ; function/resources names
-
 ;; Variables
+;; FIXME
+(defcustom ncl-doc-baseurl
+  "http://www.ncl.ucar.edu/Document/"
+  "Ncl documentation website base url. To construct URLs individual pages"
+  :group 'ncl-doc
+  :type 'string)
+
+(defcustom ncl-doc-cache-dir
+  "~/.ncl-doc/"
+  "Directory to store the cache"
+  :group 'ncl-doc
+  :type 'string)
 
 (define-minor-mode ncl-doc
   "Minor mode to help to read on line documentation of ncl
@@ -29,8 +40,23 @@
 (defvar ncl-doc-mode-hook nil
   "hook runs after enabling the ncl-doc-mode")
 
-(defvar ncl-doc-mode-map nil)
+(defvar ncl-doc-mode-map nil
+  "key bindings")
 
+;;; functions
+(defun ncl-doc-cache-dir-create ()
+  "creates cache dir"
+  (interactive)
+  (unless (file-directory-p ncl-doc-cache-dir)
+    (make-directory ncl-doc-cache-dir)))
+
+(defun ncl-doc-thing-at-point ()
+  "collect the thing at point tell if its a resource"
+  (interactive)
+  (let ((tap (thing-at-point 'symbol)))
+    (message tap)))
+
+;;; construction of doc by removing header
 
 (provide 'ncl-doc)
 ;;; ncl-doc.el ends here
