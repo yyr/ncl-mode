17a6ca2e78cfba477a510f438f39b2b4987da9bf
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-12-12 02:13:11 +0900

    * ncl-helpers.el: placed ncl-doc major mode code in ncl-helpers.el

--- ncl-helpers.el
+++ ncl-helpers.el
@@ -33,39 +33,6 @@
   :group 'ncl-indent)
 
 
-(defcustom ncl-tab-mode-default nil
-  "Default tabbing/carriage control style for empty files in NCL mode.
-A non-nil value specifies tab-digit style of continuation control.
-A value of nil specifies that continuation lines are marked
-with a character in column 6."
-  :type  'boolean
-  :safe  'booleanp
-  :group 'ncl-indent)
-
-(defcustom ncl-continuation-string \\\\
-  "Single-character string used for NCL continuation lines.
-In fixed format continuation style, this character is inserted in
-column 6 by \\[ncl-split-line] to begin a continuation line.
-Also, if \\[ncl-indent-line] finds this at the beginning of a
-line, it will convert the line into a continuation line of the
-appropriate style.  Normally \\."
-  :type  'string
-  :safe  (lambda (value) (and (stringp value) (= (length value) 1)))
-  :group 'ncl)
-
-(defcustom ncl-comment-region ";;$"
-  "String inserted by \\[ncl-comment-region] at start of each line in region."
-  :type  'string
-  :safe  'stringp
-  :group 'ncl-indent)
-
-(defcustom ncl-indented-comment-re ";"
-  "Regexp matching comments to indent as code."
-  :type  'regexp
-  :safe  'stringp
-  :group 'ncl-indent)
-
-
 ;; User options end here.
 (easy-menu-define ncl-menu ncl-mode-map "Menu for NCL mode."
   `("NCL"
@@ -97,5 +64,69 @@ appropriate style.  Normally \\."
      :active   (not (lookup-key (current-local-map) [menu-bar index]))
      :included (fboundp 'imenu-add-to-menubar)
      :help "Add an index menu to the menu-bar"]))
+
+;;=================================================================
+;; Define major mode
+;;=================================================================
+(defvar ncl-doc-mode-hook nil
+  "hook runs after enabling the ncl-doc-mode")
+(defvar ncl-doc-return-window-config nil
+  "previous window config")
+
+(defvar ncl-doc-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [mouse-1]  'ncl-doc-lookup-and-leave)
+    (define-key map [mouse-2]  'ncl-doc-lookup)
+    (define-key map "\C-m"     'ncl-doc-lookup-and-leave)
+    (define-key map " "        'ncl-doc-lookup-and-leave)
+    (define-key map "f"        'ncl-doc-lookup)
+    (define-key map "q"        'ncl-doc-quit-window)
+    (define-key map "n"        'ncl-doc-next-line)
+    (define-key map "p"        'ncl-doc-prev-line)
+
+    (define-key map "/"        'isearch-forward)
+    (define-key map "l"        'recenter)
+    (define-key map "<"        'beginning-of-buffer)
+    (define-key map ">"        'end-of-buffer)
+    (define-key map "v"        'scroll-down)
+    map)
+  "Keymap for `ncl-doc-mode-mode'.")
+
+(define-derived-mode ncl-doc-mode fundamental-mode "ncl-doc"
+  "major mode to help read NCL docs from UCAR website"
+  (kill-all-local-variables)
+  (use-local-map ncl-doc-mode-map)
+  (setq buffer-read-only t)
+  (run-mode-hooks))
+
+(defun ncl-doc-move-prev-line ()
+  "Move to previous entry"
+  (interactive)
+  (when (< 3 (line-number-at-pos))
+    (call-interactively 'previous-line)))
+
+(defun ncl-doc-next-line ()
+  "Move to next entry"
+  (interactive)
+  (when (< (line-number-at-pos)
+           (- (line-number-at-pos (point-max)) 1))
+    (call-interactively 'next-line)))
+
+(defun ncl-doc-lookup ()
+  "Lookup the current line in a browser."
+  (interactive)
+
+  (let ((url (get-text-property (point) 'ncl-doc-target-url)))
+    (if url
+        (progn
+          (beginning-of-line)
+          (message "Browsing: \"%s\"" url)
+          (browse-url url))
+      (error "No URL on this line"))))
+
+(defun ncl-doc-quit-window ()
+  "Leave the completions window."
+  (interactive)
+  (set-window-configuration ncl-doc-return-window-config))
 
 ;;; ncl-helpers.el ends here
