afdc83d7c3bc99b84d439ae91c65fd4f159ce4c3
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-06-24 16:47:53 +0900

    * inf-ncl.el: mostly done writing, No tests though

--- inf-ncl.el
+++ inf-ncl.el
@@ -1,6 +1,6 @@
 ;;; inf-ncl.el --- Inferior NCL mode
 
-;; Copyright (C) 2011-2012 Yagnesh Raghava Yakkala <http://yagnesh.org>
+;; Copyright (C) 2012 Yagnesh Raghava Yakkala <http://yagnesh.org>
 
 ;; Author: Yagnesh Raghava Yakkala <hi@yagnesh.org>
 ;; URL: https://github.com/yyr/ncl.el
@@ -21,7 +21,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with python.el.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -30,7 +30,7 @@
 
 ;; Put the following in the .emacs file
 ;;   (require 'inf-ncl)
-;; Then call `M-x inf-ncl'
+;; Then call `M-x run-ncl'
 
 ;;; Code:
 
@@ -66,47 +66,45 @@
 ;;=================================================================
 (defvar inf-ncl-ncl "ncl")
 
-(defconst inf-ncl-error-regexp-alist
-  '())
+(defvar inf-ncl-buffer nil
+  "*The current ncl process buffer.")
 
-(defvar inf-ncl-buffer nil "*The current ncl process buffer.")
-
-(defvar inf-ncl-mode-map nil
-  "*Mode map for inf-ncl-mode")
-
-(cond ((not inf-ncl-mode-map)
-       (setq inf-ncl-mode-map
-             (copy-keymap comint-mode-map))
-       (define-key inf-ncl-mode-map "\C-c\C-l" 'ncl-load-file)))
-
-;; keys for communication from ncl mode buffer
-(define-key ncl-mode-map "\C-x\C-e" 'inf-ncl-send-line)
-(define-key ncl-mode-map "\C-c\C-r" 'inf-ncl-send-region)
-(define-key ncl-mode-map "\C-c\M-r" 'inf-ncl-send-region-and-go)
-(define-key ncl-mode-map "\C-c\C-l" 'inf-ncl-load-file)
-(define-key ncl-mode-map "\C-c\C-y" 'inf-ncl-switch-to-ncl)
-(define-key ncl-mode-map "\C-c\C-z" 'inf-ncl-switch-to-end-ncl)
-(define-key ncl-mode-map "\C-c\C-n" 'run-ncl)
+(defvar inf-ncl-mode-map
+  (let ((map (copy-keymap comint-mode-map)))
+    (define-key map "\C-c\C-l" 'ncl-load-file)
+    map)
+  "*Mode map for inf-ncl-mode.")
 
 (defvar inf-ncl-font-lock-keywords)
 
-(define-derived-mode inf-ncl-mode comint-mode "Inferior NCL mode"
-  "Major mode for interacting with an inferior NCL interpreter.
+(defconst inf-ncl-error-regexp-alist
+  '())
 
-The following commands are available:
-\\{inf-ncl-mode-map}
+(defvar inf-ncl-ncarg-root (getenv "NCARG_ROOT")
+  "NCARG_ROOT from envronemt")
+
+(defvar inf-ncl-lib-root
+  (concat (directory-file-name inf-ncl-ncarg-root) "/lib/ncarg/nclscripts/")
+  "Ncl builtin library root")
+
+(defvar inf-ncl-prev-dir/file nil
+  "Catches the last loaded library or file.")
+
+;;=================================================================
+;; Define major mode
+;;=================================================================
+
+(define-derived-mode inf-ncl-mode comint-mode "*Inf NCL*"
+  "Major mode for interacting with an inferior NCL interpreter.
 
 One major advantage of using inferior mode is to be able to send
 pieces of code from other buffers to the interpreter. In this
 case from ncl mode buffer to background running ncl process.
 
 For that, The following functions are available for you.
- `run-ncl'
- `inf-ncl-load-file'
-
+`run-ncl'
+`inf-ncl-load-file'
 `inf-ncl-switch-to-ncl'
-`inf-ncl-switch-to-end-ncl'
-
 `inf-ncl-send-line'
 `inf-ncl-send-region'
 `inf-ncl-send-region-and-go'
@@ -114,34 +112,99 @@ For that, The following functions are available for you.
 Customizations: Edit
    `comint-mode-hook'  => before entering into inf-ncl-mode (for ex: custom key commands)
    `inf-ncl-mode-hook'  => after entering into inf-ncl-mode. (for ex: activation minor modes)
+
+The following commands are available:
+\\{inf-ncl-mode-map}
 "
   (setq comint-prompt-regexp inf-ncl-prompt-pattern-regexp))
 
+;;=================================================================
+;; Functions
+;;=================================================================
 
-(defun inf-ncl-input-filter (str)
-  "Don't save anything matching `inf-ncl-input-filter-regexp'."
-  (not (string-match inf-ncl-input-filter-regexp str)))
+(defun inf-ncl-keys ()
+  "Key for inferior mode interaction from ncl buffer."
+  ;; keys for communication from ncl mode buffer
+  (define-key ncl-mode-map "\C-x\C-e" 'inf-ncl-send-line)
+  (define-key ncl-mode-map "\C-c\C-r" 'inf-ncl-send-region)
+  (define-key ncl-mode-map "\C-c\M-r" 'inf-ncl-send-region-and-go)
+  (define-key ncl-mode-map "\C-c\C-l" 'inf-ncl-load-file)
+  (define-key ncl-mode-map "\C-c\C-y" 'inf-ncl-switch-to-ncl)
+  (define-key ncl-mode-map "\C-c\C-n" 'run-ncl))
+
+(defun inf-ncl-proc ()
+  "Returns current inferior process."
+  (or (get-buffer-process (if (eq major-mode 'inf-ncl-mode)
+                              (current-buffer)
+                            inf-ncl-buffer))
+      (error "No ncl process is running. Start one \"M-x run-ncl\" ")))
+
+;; FIXME seems not working (bug in comint-mode?)
+;;;###autoload
+(defun inf-ncl-load-file (fn)
+  "Load ncl library into inferior process."
+  (interactive (comint-source-default "Load ncl library: "
+                                      inf-ncl-prev-dir/file nil nil))
+  (comint-check-source fn)              ;checks if the buffer needs to be saved
+  (setq inf-ncl-prev-dir/file (cons (file-name-directory fn)
+                                    (file-name-nondirectory fn)))
+  (comint-send-string (inf-ncl-proc) (concat "load \""
+                                             fn
+                                             "\"\n")))
+
+;;;###autoload
+(defun inf-ncl-switch-to-ncl (eob-p)
+  "Switch to inferior process. with EOB-P go to the end of the buffer."
+  (interactive "P")
+  (if (get-buffer inf-ncl-buffer)
+      (pop-to-buffer inf-ncl-buffer)
+    (error "No inferior process is running."))
+  (cond (eob-p
+         (push-mark)
+         (goto-char (point-max)))))
+
+;;;###autoload
+(defun inf-ncl-send-line (switch)
+  "Send a line to inferior process. With prefix SWITCH switches
+  to the process buffer"
+  (interactive "P")
+  (let ((ln (buffer-substring-no-properties
+             (line-beginning-position) (line-end-position))))
+    (comint-send-string (inf-ncl-proc) (concat ln "\n")))
+  (if switch
+      (inf-ncl-switch-to-ncl t)))
+
+;;;###autoload
+(defun inf-ncl-send-region (start end)
+  "Send a region to inferior ncl process."
+  (interactive "r")
+  (comint-send-region (inf-ncl-proc) start end))
+
+(defun inf-ncl-send-region-and-go (start end)
+  "Send a region to inferior ncl process and switch buffer."
+  (comint-send-region (inf-ncl-proc) start end)
+  (inf-ncl-switch-to-ncl t))
+
+;;;###autoload
+(defun inf-ncl (arg)
+  "Run inferior ncl process.  With prefix ARG interactively
+choose the interpreter"
+  (interactive (list (if current-prefix-arg
+                         (read-string "Run ncl: " ncl-shell-program)
+                       ncl-shell-program)))
+  (if (not (comint-check-proc inf-ncl-buffer))
+      (progn
+        (set-buffer (apply 'make-comint "ncl" arg nil))
+        (inf-ncl-mode)))
+  (setq ncl-shell-program arg)
+  (pop-to-buffer (setq inf-ncl-buffer (format "*%s*" "ncl"))))
 
 (defalias 'run-ncl 'inf-ncl)
 
-;;=================================================================
-;; internal variables
-;;=================================================================
-
 ;;;###autoload
-(defun inf-ncl (cmd)
-  "Run inferior ncl process. with prefix CMD arg choose "
-  (interactive (list (if current-prefix-arg
-                         (read-string "Run ncl:" ncl-shell-program)
-                       ncl-shell-program)))
-  (if (not (comint-check-proc "*ncl*"))
-      (progn
-        (set-buffer (apply 'make-comint "ncl" cmd nil))
-        (inf-ncl-mode)))
-  (setq ncl-shell-program cmd)
-  (setq ncl-buffer "*ncl*")
-  (pop-to-buffer "*ncl*"))
-
+(eval-after-load 'ncl-mode
+  ;; load the keys for ncl
+  '(add-hook 'ncl-mode-hook 'inf-ncl-keys))
 
 (provide 'inf-ncl)
 ;;; inf-ncl.el ends here
