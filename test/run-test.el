d76e640f128bb493d5c6d39741d7f65cf999f745
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-09-17 05:46:38 +0900

    * test/run-test.el: ert as sub module for emacs23

--- test/run-test.el
+++ test/run-test.el
@@ -4,8 +4,9 @@
 ;;  emacs -batch -Q -l test/run-test.el
 
 
-(defvar ncl-test-dir (file-name-directory load-file-name))
-(defvar ncl-root-dir (concat ncl-test-dir ".."))
+(defvar ncl-test-dir (file-name-directory (or load-file-name
+                                              buffer-file-name)))
+(defvar ncl-root-dir (file-name-as-directory (concat ncl-test-dir "..")))
 
 
 ;;; load-path
@@ -14,6 +15,15 @@
 
 ;; load tests
 (load "ncl-mode-tests")
+
+
+;; Use ERT from github when this Emacs does not have it
+(unless (locate-library "ert")
+  (add-to-list
+   'load-path
+   (file-name-directory (concat ncl-root-dir "/lib/ert/lisp/emacs-lisp")))
+  (require 'ert-batch)
+  (require 'ert-ui))
 
 ;;; run
 (if noninteractive
