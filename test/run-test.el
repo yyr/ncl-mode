c1b6f7b694e12c2454be16b0fdafcf02f825874d
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-09-17 06:10:10 +0900

    * .travis.yml: emacs23 is taken off. (seems emacs23 doesn't even have   prog-mode) (may be later)

--- test/run-test.el
+++ test/run-test.el
@@ -6,24 +6,24 @@
 
 (defvar ncl-test-dir (file-name-directory (or load-file-name
                                               buffer-file-name)))
-(defvar ncl-root-dir (file-name-as-directory (concat ncl-test-dir "..")))
+(defvar ncl-root-dir (file-truename  (concat ncl-test-dir "..")))
 
-
+(setq debug-on-error t)
 ;;; load-path
 (mapc (lambda (p) (add-to-list 'load-path p))
       (list ncl-test-dir ncl-root-dir))
-
-;; load tests
-(load "ncl-mode-tests")
 
 
 ;; Use ERT from github when this Emacs does not have it
 (unless (locate-library "ert")
   (add-to-list
    'load-path
-   (file-name-directory (concat ncl-root-dir "/lib/ert/lisp/emacs-lisp")))
+   (file-name-as-directory (concat ncl-root-dir "/lib/ert/lisp/emacs-lisp")))
   (require 'ert-batch)
   (require 'ert-ui))
+
+;; load tests
+(load "ncl-mode-tests")
 
 ;;; run
 (if noninteractive
