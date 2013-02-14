9126568993cd7aaedeac9b9d04e30645ff04d485
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-02-15 04:17:59 +0900

    * tools/get-tags.sh: update & cleanup
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- tools/get-tags.sh
+++ tools/get-tags.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 #
 # Copyright (C) 2012, 2013 Yagnesh Raghava Yakkala. http://yagnesh.org
-#    File: ncl-ctags-gen.sh
+#    File: gen-tags.sh
 # Created: Monday, October 24 2011
 # License: GPL v3 or later.  <http://www.gnu.org/licenses/gpl.html>
 #
 
 # Description:
-# to generate TAGS for ncl source files; -e option to produce emacs format
+# Generates TAGS file for ncl source files;
+# -e option is to produce emacs format
 # NOTE: may fail if the filename has spaces
-# usage: ncl-ctags-gen.sh /path/to/ncl/files
 
-ctags-exuberant -e -a --verbose=yes  --langdef=ncl \
+# USAGE: ctags-gen.sh /path/to/ncl/files
+
+function tag_gen()
+{
+ctags -e -a --verbose=yes  --langdef=ncl \
     --langmap=ncl:.ncl --regex-ncl='/^[[:space:]]*function[[:space:]]+([a-zA-Z0-9_]+)[:blank:]*.*/\1/f,function/' \
     --regex-ncl='/^[[:space:]]*procedure[[:space:]]+([a-zA-Z0-9_]+)[:blank:].*/\1/p,procedure/' `find ${1:-"."} -type f -name "*.ncl"`
+}
 
-# ncl-ctags-gen.sh ends here
+tag_gen ${1:-"."}
+
+# gen-tags.sh ends here
