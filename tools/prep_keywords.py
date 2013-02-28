cfbff5b92e5ee59cc8517e459d326989c0aa5a4f
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-02-28 14:01:05 +0900

    * tools/prep_keywords.py: bare one.
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

---
+++ tools/prep_keywords.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+Download and collect Ncl keywords from Ncl website
+'''
+
+DATE     = "Friday, February 15 2013"
+AUTHOR   = "Yagnesh Raghava Yakkala"
+WEBSITE  = "http://yagnesh.org/yyr/ncl-mode"
+LICENSE  = "GPL v3 or later"
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
